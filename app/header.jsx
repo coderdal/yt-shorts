f67b94ee2088428e8a08186e079cc2e201372d3b
coderdal <agircoder@gmail.com>
2023-04-12 23:27:02 +0300

    react-icons pkg added - header logo section added

--- app/header.jsx
+++ app/header.jsx
@@ -1,6 +1,26 @@
 import React from "react";
 import styles from "@/styles/header.module.css";
 
+import { CgMenu } from "react-icons/cg";
+import Logo from "@/app/logo.jsx";
+
+import Link from "next/link";
+
 export default function Header() {
-  return <header className={styles.header}>Header</header>;
+  return (
+    <header className={styles.header}>
+      <div className={styles.appLogo}>
+        <button className={styles.menuButton}>
+          <CgMenu size={23} />
+        </button>
+        <div className={styles.logo}>
+          <div className={styles.image}>
+            <Link href={"/"} className={styles.link}>
+              <Logo />
+            </Link>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 }
