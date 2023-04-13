13a48484e4c4b3a7431d1747f19a6f3479ad209a
coderdal <agircoder@gmail.com>
2023-04-13 20:34:48 +0300

    action buttons added

--- app/header.jsx
+++ app/header.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import styles from "@/styles/header.module.css";
 
-import { CgMenu } from "react-icons/cg";
+import { CgMenu, CgProfile } from "react-icons/cg";
+import { RiVideoAddLine } from "react-icons/ri";
+import { IoMdNotificationsOutline } from "react-icons/io";
+
 import Logo from "@/app/logo.jsx";
 
 import Link from "next/link";
@@ -13,6 +16,7 @@ export default function Header() {
         <button className={styles.menuButton}>
           <CgMenu size={23} />
         </button>
+
         <div className={styles.logo}>
           <div className={styles.image}>
             <Link href={"/"} className={styles.link}>
@@ -20,6 +24,20 @@ export default function Header() {
             </Link>
           </div>
         </div>
+      </div>
+
+      <div className={styles.searchContainer}>search bar</div>
+
+      <div className={styles.actions}>
+        <button className={styles.action}>
+          <RiVideoAddLine size={24} />
+        </button>
+        <button className={styles.action}>
+          <IoMdNotificationsOutline size={24} />
+        </button>
+        <button className={styles.action}>
+          <CgProfile size={28} />
+        </button>
       </div>
     </header>
   );
