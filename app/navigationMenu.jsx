d31adb035f3220e919e685c8d3c2ec499cc8bd7d
coderdal <agircoder@gmail.com>
2023-04-14 00:28:20 +0300

    navigation bar styles

--- app/navigationMenu.jsx
+++ app/navigationMenu.jsx
@@ -2,6 +2,54 @@ import React from "react";
 
 import styles from "@/styles/navigationMenu.module.css";
 
+import { CgHome } from "react-icons/cg";
+import { BiMovie } from "react-icons/bi";
+import { SlSocialLinkedin } from "react-icons/sl";
+import { FiGithub } from "react-icons/fi";
+import {
+  MdOutlineSubscriptions,
+  MdOutlineFeaturedPlayList,
+} from "react-icons/md";
+
 export default function NavigationMenu() {
-  return <nav className={styles.nav}>NavigationMenu</nav>;
+  return (
+    <nav className={styles.nav}>
+      <ul className={styles.items}>
+        <li className={styles.item}>
+          <CgHome size={22} />
+          <span>Home</span>
+        </li>
+        <li className={styles.item}>
+          <BiMovie size={22} />
+          <span>Shorts</span>
+        </li>
+        <li className={styles.item}>
+          <MdOutlineSubscriptions size={22} />
+          <span>Subscriptions</span>
+        </li>
+
+        <a
+          href="https://github.com/coderdal"
+          target="_blank"
+          className={styles.link}
+        >
+          <li className={styles.item}>
+            <FiGithub size={22} />
+            <span>Github</span>
+          </li>
+        </a>
+
+        <a
+          href="https://www.linkedin.com/in/muhammederdal/"
+          target="_blank"
+          className={styles.link}
+        >
+          <li className={styles.item}>
+            <SlSocialLinkedin size={22} />
+            <span>Linkedin</span>
+          </li>
+        </a>
+      </ul>
+    </nav>
+  );
 }
