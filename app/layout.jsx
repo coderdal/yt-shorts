2b1eed311ff8ee134bac2114781a0e80f4dd1c25
coderdal <agircoder@gmail.com>
2023-04-16 16:04:04 +0300

    error msgs removed

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,11 +1,5 @@
 import React from "react";
 
 export default function layout({ children }) {
-  return (
-    <html lang="en">
-      <body>
-        <main>{children}</main>
-      </body>
-    </html>
-  );
+  return <>{children}</>;
 }
